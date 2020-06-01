2f894cbc0c42fde0239fd30d9246fbaef9755c03
Alan Dawidowicz <alan.dawidowicz@mediamonks.com>
2020-06-01 17:43:49 -0300

    Update and clean bannerToVideo feature

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ const path = require('path');
 const mainQuestions = require('./questions/mainQuestions');
 const backupImageTask = require('./tasks/backupImageTask');
 const optimizeBuildedBannerTask = require('./tasks/optimizeBuildedBannerTask');
-const exportTask = require('./tasks/exportTask');
+const bannerToVideo = require('./tasks/bannerToVideo');
 
 global.mainTask = null;
 global.exportConfig = null;
 global.appRoot = path.resolve(__dirname);
+global.folderRoot = path.resolve();
 
 (async () => {
     await mainQuestions();
 
     switch(global.mainTask) {
-        case 'exportBanner':
-            await exportTask();
+        case 'export':
+            await bannerToVideo();
             break;
         case 'backupImage':
             await backupImageTask();
---
+++ src/tasks/bannerToVideo/index.js
@@ -0,0 +1,42 @@
+const exportQuestions = require('../../questions/exportQuestions');
+const puppeteer = require('puppeteer');
+const { record } = require('./record');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+
+const exportTask = async () => {
+    await exportQuestions();
+
+    const outputFolder = 'banner_frames';
+    const config = global.exportConfig;
+    const size = config.size.split('x');
+
+    await puppeteer.launch({headless : true}).then(async browser => {
+        const page = await browser.newPage();
+
+        await page.setViewport({ width: Number(size[0]), height: Number(size[1])} );
+    
+        await page.goto(`file://${process.cwd()}/index.html`, {waitUntil : 'networkidle2'});
+    
+        await record({
+            ffmpeg: ffmpegPath,
+            browser: browser,
+            page: page,
+            output: 'OUTPUT.webm',
+            fps: 5,
+            frames: 120,
+            prepare: function (browser, page) {
+                console.log("Generating video, please wait...");
+            },
+            render: function (browser, page, frame) {}
+        });
+
+        // TODO: Dispath event
+    
+        await browser.close();
+    });
+
+    console.log("Finished");
+    return;    
+}
+
+module.exports = exportTask
