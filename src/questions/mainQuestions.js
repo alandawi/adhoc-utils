1e4e6788a3aec08df1f5a7aa274df0b6c7e1551b
Alan Dawidowicz <alan.dawidowicz@mediamonks.com>
2020-06-02 20:00:57 -0300

    Clean code

--- src/questions/mainQuestions.js
+++ src/questions/mainQuestions.js
@@ -7,11 +7,10 @@ const mainQuestions = async () => {
             name: 'task',
             message: 'Beautiful day to perform tasks! What do you need?',
             choices: [
-                { title: 'Transform banner to video', value: 'export' },
+                { title: 'Transform banner to video', value: 'bannerToVideo' },
                 { title: 'Generate a backup image from a banner', value: 'backupImage' },
-                { title: 'Extract frames of a video', value: 'extractFrames' },
-                { title: 'Optimize a builded banner', value: 'optimizeBuildedBanner' },
-                { title: 'Another option....', value: 'another' },
+                { title: 'Generate frames from a banner', value: 'extractFrames' },
+                { title: 'Optimize a builded banner', value: 'optimizeBuildedBanner' }
             ],
             initial: 0
         },
